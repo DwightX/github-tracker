1915923045d9bed2665007bf187175767189dcee
Dwight <dwight@Marketing-1-iMac.local>
2021-10-06 16:41:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    users:[],
+    loading: false
+  }
+
+  async componentDidMount(){
+    // sets state to true
+    this.setState({loading:true});
+    //makes request and gets response from api
+   const res = await axios.get('https://api.github.com/users');
+    // gets data from request and sets state back to false
+   this.setState({users:res.data,loading:false})
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <Navbar/>
+        <div className="container">
+          {/* passing in loading and users props */}
+        <Users loading={this.state.loading} users={this.state.users}/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
+ 
