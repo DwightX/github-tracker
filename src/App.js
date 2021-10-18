ac124def4745fa08537207af4d8df2ae3a75434f
Dwight <dwight@Marketing-1-iMac.local>
2021-10-18 14:15:33 -0400

    added repos to profiles

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ class App extends Component{
   state = {
     users:[],
     user:{},
+    repos:[],
     loading: false,
-    alert: null
-  }
+    alert: null  }
     // searching github users
   searchUsers = async text => {
     this.setState({loading:true});
@@ -31,6 +31,13 @@ class App extends Component{
         const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
        this.setState({user:res.data, loading:false})
     };
+    //get user repos
+    getUserRepos = async username => {
+      this.setState({loading:true});
+
+      const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+     this.setState({repos:res.data, loading:false})
+  };
     //Clear users from state
     clearUsers = () => this.setState({ users: [], loading:false });
     //Set Alert
@@ -40,7 +47,7 @@ class App extends Component{
     }
 
   render(){
-    const {loading,users, user} = this.state;
+    const {loading,users,user,repos} = this.state;
     return(
       <Router>
       <div className="App">
@@ -68,7 +75,9 @@ class App extends Component{
               <User 
               {...props} 
               getUser={this.getUser} 
-              user={user} 
+              getUserRepos={this.getUserRepos} 
+              user={user}
+              repos={repos}
               loading={loading}
               />
             )}/>
